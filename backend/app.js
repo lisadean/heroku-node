79b6c6d59fe2bd3036fe66375363d6011a663e85
Lisa Dean <lisa@lisadean.net>
2019-04-10 15:32:33 -0400

    Added env vars to return data

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,10 @@ app.use(express.json());
 
 
 app.get('*', (req, res) => {
-  res.send(`TADA! Backend stuff and things! Running on ${process.env.HOST}`);
+  res.send(`TADA! Backend stuff and things! Running on ${process.env.HOST}<br>
+    BUILD_DIR: ${process.env.BUILD_DIR}<br>
+    CACHE_DIR: ${process.env.CACHE_DIR}<br>
+    PROJECT_PATH: ${process.env.PROJECT_PATH}`);
 });
 
 app.listen(port, () => {
