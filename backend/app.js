1b37d8263a57992de080ba6c41c0b242e0868e07
Lisa Dean <lisa@lisadean.net>
2019-04-10 15:46:39 -0400

    Changed variables around

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,20 @@
 require('dotenv').config();
 const express = require('express');
 const port = process.env.PORT || 3000;
+const NODE_ENV = process.env.NODE_ENV || 'dev';
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
 app.get('*', (req, res) => {
-  res.send(`TADA! Backend stuff and things! Running on ${process.env.HOST}<br>
-    BUILD_DIR: ${process.env.BUILD_DIR}<br>
-    CACHE_DIR: ${process.env.CACHE_DIR}<br>
-    PROJECT_PATH: ${process.env.PROJECT_PATH}`);
+  res.send(
+    `TADA! Backend stuff and things!<br><br>
+    STUFF: ${process.env.STUFF}<br>
+    NODE_ENV: ${NODE_ENV}<br>
+    PROJECT_PATH: ${process.env.PROJECT_PATH}<br>
+    PORT: ${port}`
+  );
 });
 
 app.listen(port, () => {
